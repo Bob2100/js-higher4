32555e9c354cc4847b3b74b8101ba25a884d8f5f
Bob <bob_w@qq.com>
2021-02-18 21:28:41 +0800

    for循环每次执行都是一个全新的块作用域

--- closure.js
+++ closure.js
@@ -1,13 +1,26 @@
 //var 声明的是全局变量，最后i的值是10
-for (var i = 0; i < 10; i++) {
-  setTimeout(() => {
-    console.log(i);
-  }, 1000);
-}
+// for (var i = 0; i < 10; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000);
+// }
 
-//let声明的是局部变量，以闭包的形式被里面的异步函数访问到
-for (let i = 0; i < 10; i++) {
-  setTimeout(() => {
-    console.log(i);
-  }, 1000);
+//JS中的for循环体比较特殊，每次执行都是一个全新的独立的块作用域，用let声明的变量传入到 for循环体的作用域后，不会发生改变
+// for (let i = 0; i < 10; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000);
+// }
+
+function createFn() {
+  var arr = [];
+  for (var i = 0; i < 3; ++i) {
+    arr[i] = function () {
+      return i;
+    }
+  }
+  // console.log(i);
+  return arr;
 }
+var arrFn = createFn();
+console.log(arrFn[0]());// 3
