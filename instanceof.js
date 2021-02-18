a7a91ec5dca7813f5ee2c985a738131dbd576175
Bob <bob_w@qq.com>
2021-02-18 20:23:15 +0800

    如何判断一个对象是非函数的对象

--- instanceof.js
+++ instanceof.js
@@ -25,3 +25,11 @@ console.log(typeof sayHi);//function
 //instanceof是通过原型的constructor来判断的
 person.__proto__ = null;
 console.log(person instanceof Object);//false
+
+//如何判断一个对象是非函数的对象
+function isObjectNotFunction(obj) {
+  return (obj instanceof Object) && (typeof obj != 'function')
+}
+console.log(isObjectNotFunction(sayHi));//false
+console.log(isObjectNotFunction(arr));//true
+console.log(isObjectNotFunction({}));//true
